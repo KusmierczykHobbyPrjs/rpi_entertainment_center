e098d811968180560b81b0a43b86433c7941cd51
tkusmierczyk <tomasz.kusmierczyk@gmail.com>
2024-02-14 12:01:25 +0200

    Make sure only one instance of port forwarding is running.

--- port_forwarding.sh
+++ port_forwarding.sh
@@ -4,5 +4,25 @@
 # Installation:
 # > sudo apt-get install socat
 
+#######################################################################
+
+# Lock file path
+LOCK_FILE="/tmp/port_forwarding.lock"
+
+# Check if the lock file exists
+if [ -f "$LOCK_FILE" ]; then
+    # echo "Another instance of the script is already running."
+    exit
+else
+    # Create a lock file
+    touch "$LOCK_FILE"
+fi
+
+# Ensure that the lock file is removed when the script exits
+trap "rm -f $LOCK_FILE" EXIT
+
+#######################################################################
+
+
 # Forwards port=8282 => 192.168.1.20:8080
 socat tcp-listen:8282,fork,reuseaddr tcp:192.168.1.20:8080 
